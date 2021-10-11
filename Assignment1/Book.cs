c20fa94d4d1f7e01e17172d929a62bb570312aa4
Ziaullah Hassan <Ziaullah Hassan@DESKTOP-2U19JOA>
2021-10-11 12:39:27 -0700

    Assignment 1 -Adding Constructor

--- Assignment1/Book.cs
+++ Assignment1/Book.cs
@@ -6,11 +6,24 @@ using System.Threading.Tasks;
 
 namespace Assignment1
 {
-    class Book
+  public  class Book
     {
         public String Title { get; set; }
         public String Author { get; set; }
         public int PageNumber { get; set; }
-        public int ISBN13 { get; set; }
+        public String ISBN13 { get; set; }
+
+        public Book()
+        {
+
+        }
+
+        public Book(String title,String author,int pagenumber,String isbn13)
+        {
+            this.Title = title;
+            this.Author = author;
+            this.PageNumber = pagenumber;
+            this.ISBN13 = isbn13;
+        }
     }
 }
